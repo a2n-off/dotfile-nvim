c1086f27bb7a109ca1374c5eacc4aaf1dfbdad82
a2n <a2n.dev@pm.me>
2022-08-19 16:16:59 +0200

    modules

---
+++ lua/a2n/highlights.lua
@@ -0,0 +1,9 @@
+print('📜 highlights.lua loaded')
+
+-- theme configuration
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.winblend = 0
+vim.opt.wildoptions = 'pum'
+vim.opt.pumblend = 5
+vim.opt.background = 'dark'
